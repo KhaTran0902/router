83bb5f7e1f19b5a7b4aa89c7391d1abcd063d41f
KhaTran0902 <khatran090201@gmail.com>
2023-05-09 21:15:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -9,29 +9,41 @@ import Prices from "./pages/Prices";
 import NotFound from "./pages/NotFound";
 import Product from "./pages/Product";
 export default function App() {
+  const Header = ()=>{
+    const myStyle ={
+      color:"white",
+      backgroundColor: "DodgerBlue",
+      padding:"10px",
+      fontFamily:"Sans-Serif"
+    }
+  }
   return (
-    <div className="App">
-      {/* <Weather/> */}
-      {/* <TestUseEffect/> */}
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/product">Products</Link>
-        </li>
-        <li>
-          <Link to="/prices">Prices</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/product" element={<Products />}>
-          <Route path=":id" element={<Product />}></Route>
-        </Route>
-        <Route path="/prices" element={<Prices />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+    // <div className="App">
+    //   {/* <Weather/> */}
+    //   {/* <TestUseEffect/> */}
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/product">Products</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/prices">Prices</Link>
+    //     </li>
+    //   </ul>
+    //   <Routes>
+    //     <Route path="/" element={<Home />}></Route>
+    //     <Route path="/product" element={<Products />}>
+    //       <Route path=":id" element={<Product />}></Route>
+    //     </Route>
+    //     <Route path="/prices" element={<Prices />}></Route>
+    //     <Route path="*" element={<NotFound />}></Route>
+    //   </Routes>
+    // </div>
+    <div>
+      <h1 style={myStyle}>Hello Style</h1>
+      
     </div>
-  );
+  )
 }
